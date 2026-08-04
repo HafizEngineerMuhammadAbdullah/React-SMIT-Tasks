093dca1cc4177cb79c8711d16f013e6f2f8f2fa3
HafizEngineerMuhammadAbdullah <abdullahkhalid2k25@gmail.com>
2026-08-04 15:21:15 -0700

    Working on Signup Form

--- Firebase-Form-Project/src/components/authentication/SignupForm.jsx
+++ Firebase-Form-Project/src/components/authentication/SignupForm.jsx
@@ -73,9 +73,59 @@ const SignupForm = () => {
         </div>
       </div>
 
-       {/* RIGHT PANEL */}
-      <div className={`${styles.rightPanel} flex flex-col justify-between bg-[#2C2C2A] p-9`}>
+      {/* RIGHT PANEL */}
+      <div className={`${styles.rightPanel} w-full flex flex-col justify-evenly bg-[#2C2C2A] p-9`}>
+        <div>
+          <h1 className='font-medium text-2xl text-white text-wrap my-3'>Create your account</h1>
+          {/* <p className='text-[#9595B6] text-sm'>Already have an account? <span className='text-[#534AB7]'>Sign in</span></p> */}
+          <p className='text-[#9595B6] text-sm'>Step 1 of 3 — takes less than 2 minutes</p>
+        </div>
 
+        {/* User Form */}
+        <form className={`${styles.formGrid}}`}>
+
+          {/* Row-1 */}
+          <div className='flex gap-x-3 items-center'>
+            {/* for First Name */}
+            <div>
+              <label htmlFor="firstname" className='text-[#9595B6] text-sm'>First name</label>
+              <input type="text" id='firstname' className={`${styles.input}`} />
+            </div>
+
+            {/* for Last Name */}
+            <div>
+              <label htmlFor="lastname" className='text-[#9595B6] text-sm'>Last name</label>
+              <input type="text" id='lastname' className={`${styles.input}`} />
+            </div>
+          </div>
+
+          {/* Row-2 */}
+          {/* for Email */}
+          <div className='flex flex-col gap-3'>
+            <label htmlFor="email" className='text-[#9595B6] text-sm'>Email</label>
+            <input type="email" id='email' className={`${styles.input}`} />
+          </div>
+
+          {/* Row-3 */}
+          {/* for Password */}
+          <div className='flex flex-col gap-3'>
+            <label htmlFor="password" className='text-[#9595B6] text-sm'>Password</label>
+            <input type="password" id='password' className={`${styles.input}`} />
+          </div>
+
+
+          {/* Row-4 */}
+          <div className='flex gap-x-3 items-center'>
+            <input type="checkbox" className='bg-[#2c2c2a] border border-[rgba(108, 99, 255, 0.1)]'/>
+            <p className='text-[#9595B6] text-sm'>I agree to the <span className='text-[#534AB7]'>Terms of Service</span> and <span className='text-[#534AB7]'>Privacy Policy</span></p>
+          </div>
+          
+          {/* Row-5 */}
+          {/* Create Account */}
+          <div>
+             <input type="button" className={`${styles.input} w-full text-white text-2xl font-mono font-medium`} value="Create Account"/> 
+          </div>
+        </form>
       </div>
     </div>
   )
